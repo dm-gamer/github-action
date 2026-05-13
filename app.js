6822cb2f17034c259b634b290afcbb50d320494c
Sanjay R <sanjay.r@g3cyberspace.com>
2026-05-13 17:01:54 +0530

    git

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Docker Buddy! 🚀'));
+app.get('/', (req, res) => res.send('Hello from Docker 🚀'));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
