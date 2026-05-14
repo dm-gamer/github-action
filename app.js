04679078996bba876e792b1f947507418f278e71
Sanjay R <sanjay.r@g3cyberspace.com>
2026-05-14 12:44:23 +0530

    new-co

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Docker 🚀'));
+app.get('/', (req, res) => res.send('Hello from sanjay devops'));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
